5ba876017e2c613aed80cdc200dab99d2a73ee74
Piotrek Wielki <remmington.cruzoe@gmail.com>
2021-06-29 11:51:35 +0200

    initial edit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const wrapper = document.getElementById('wrapper');
+
+const createCircle = function(color, text) {
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+  circle.style.backgroundColor = color;
+
+  if (text || text === 0) {
+    circle.textContent = text;
+  }
+
+  wrapper.appendChild(circle);
+}
+
+
+// Task 1
+
+
+
+
+// Task 2
+
+
+
+
+// Task 3
+
+
+
+
+// Task 4
